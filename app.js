c380d7d7828e7a532a0f3cd0df8f4bee212715de
Maxim Prosvirkin <Nort359@gmail.com>
2018-02-28 13:20:59 +0300

    Finished app file.

---
+++ app.js
@@ -0,0 +1,10 @@
+const github = require('./github');
+
+const userName = process.argv[1];
+
+github.getRepos(userName, (error, repos) => {
+    if (error)
+        return console.log(`Ошибка: ${error.message}`);
+
+    repos.forEach(repo => console.log(repo.name));
+});
