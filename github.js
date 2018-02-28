8dadfff6b8d10aea980b4aa2126f5357af1180d1
Maxim Prosvirkin <Nort359@gmail.com>
2018-02-28 13:48:36 +0300

    Finished catching errors.

--- github.js
+++ github.js
@@ -1,15 +1,36 @@
 const https = require('https');
 
 function getRepos(userName, done) {
+    if (!userName) return done(new Error('Необходимо указать имя пользователя'));
+
     const options = {
         hostname: 'api.github.com',
         path: `/users/${userName}/repos`,
         headers: { 'User-Agent': 'Nort359' }
     };
 
-    https.get(options, res => {
-        console.log(res.statusCode, res.statusMessage);
+    const request = https.get(options, res => {
+        res.setEncoding('utf-8');
+
+        if (res.statusCode === 200) {
+            let body ='';
+
+            res.on('data', data => body += data);
+
+            res.on('end', () => {
+                try {
+                    const result = JSON.parse(body);
+                    done(null, result);
+                } catch (error) {
+                    done(new Error(`Не удалось обработать данные. Ошибка: (${error.message})`))
+                }
+            });
+        } else {
+            done(new Error(`Не удалось получить данные от сервера. Код: (${res.statusCode}). Сообщение: (${res.statusMessage})`));
+        }
     });
+
+    request.on('error', error => done(new Error(`Не удалось отправить запрос (${error.message})`)));
 }
 
 module.exports = {
